5321dc63811493d1fcc88b300238a052778063c8
BlacKittenx <rechellr5@gmail.com>
2024-11-13 22:17:54 +0800

    update

---
+++ php/signlog.php
@@ -0,0 +1,48 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "records";
+
+    //create connection
+    $conn = new mysqli($servername, $username,  $password, $db_name);
+
+    //check connection
+    if ($conn->connect_error)
+    {
+        die ("Connection Failed!: " . $conn->connect_error);
+    }
+
+    //Check if form was submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $userName = mysqli_real_escape_string($conn, $_POST["userName"]);
+        $email = mysqli_real_escape_string($conn, $_POST["email"]);
+        $pswd = mysqli_real_escape_string($conn, $_POST["pswd"]);
+
+        //Check if all fields are filled
+        if (!empty($userName) && !empty($email) && !empty($pswd))
+        {
+            $sql = "INSERT INTO tb_user (userName, email, pswd) VALUES (?, ?, ?)";
+
+            //Prepare statement
+            $stmt = $conn->prepare($sql);
+
+            //Bind parameters
+            $stmt->bind_param("sss", $userName, $email, $pswd);
+
+            if ($stmt->execute())
+            {
+                echo "Record inserted successfully!";
+            }
+
+            else
+            {
+                echo "ERROR: All required fields must be filled";
+            }
+        }
+
+        //connection close
+        $conn->close();
+    }
+?>
