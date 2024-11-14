cb751e6daba370ffec4dbe1de65bb784c9e7b3ef
BlacKittenx <rechellr5@gmail.com>
2024-11-14 09:12:24 +0800

    update

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<?php
+    include("php/database.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form login and register</title>
+</head>
+<body>
+    <h1>WELCOME USER</h1>
+    <a href="php/login.php">Log out</a>
+</body>
+</html>
--- php/login.php
+++ php/login.php
@@ -1,3 +1,37 @@
 <?php
-    
+    session_start();
+    include("php/database.php");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $email = mysqli_real_escape_string($conn, $_POST["email"]);
+        $pswd = mysqli_real_escape_string($conn, $_POST["pswd"]);
+
+        if (!empty($email) && !empty($pswd))
+        {
+            $query = "SELECT * FROM tb_user WHERE email = '$email' OR pswd = '$pswd'";
+            $result = mysqli_query($conn, $query);
+
+            if ($result)
+            {
+                if ($result && mysqli_num_rows($result) > 0)
+                {
+                    $user_data = mysqli_fetch_assoc($result);
+
+                    if ($user_data['pswd'] == $pswd)
+                    {
+                        header ("location: php/index.php");
+                        die;
+                    }
+                }
+            }
+
+            echo "<script> alert('Wrong username or password')</script>";
+        }
+
+        else
+        {
+            echo "<script> alert('Wrong username or password')</script>";
+        }
+    }
 ?>
